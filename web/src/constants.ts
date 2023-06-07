49e0f6801693ea76987fa0e9e96b1bba01999e21
Uchi Uchibeke <uchi.uchibeke@gmail.com>
2023-06-07 13:43:30 -0400

    add moonbeam

--- web/src/constants.ts
+++ web/src/constants.ts
@@ -1,2 +1,15 @@
-export const supportedSpendContexts = ['valora', 'evm', 'mobile', 'web', 'chimoney_redeem','status', 'metamask','walletconnect','celo', "api_call"];
-export const supportedAppStyles = ['light', 'dark', 'moonlight', 'royal'];
+export const supportedSpendContexts = [
+  "valora",
+  "evm",
+  "mobile",
+  "web",
+  "chimoney_redeem",
+  "status",
+  "metamask",
+  "walletconnect",
+  "celo",
+  "api_call",
+  "moonbeam",
+  "xumm",
+];
+export const supportedAppStyles = ["light", "dark", "moonlight", "royal"];
